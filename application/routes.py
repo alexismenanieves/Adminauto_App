aadc60e453eb04cfb64a83a9043154cd1e3e992f
Alexis Mena <alexismenanieves@hotmail.com>
2023-07-23 12:56:24 -0500

    Added API for vehicles

--- application/routes.py
+++ application/routes.py
@@ -104,13 +104,6 @@ def registro():
                            title="Registro", 
                            form=form, registro=True)
 
-@app.route("/listado")
-def listado():
-    if not session.get('edt_nom'):
-        return redirect(url_for('login'))
-    listado = Vehicles.query.order_by("veh_usg")
-    return render_template('listado.html', listado=listado)
-
 ############################ API SECTION ###############################
 
 user_schema = UserSchema()
@@ -223,30 +216,61 @@ def apiaddvehicle():
     if session.get('edt_nom') and request.method=='POST':
         curr_editor = session['edt_nom']
         now_dt  = datetime.now()
-        vehicle = Vehicles(
-            veh_lic_pla =request.form['veh_lic_pla'], 
-            veh_usg     =request.form['veh_usg'],
-            veh_mar     =request.form['veh_mar'],
-            veh_mod     =request.form['veh_mod'],
-            veh_yea     =request.form['veh_yea'],
-            veh_col     =request.form['veh_col'],
-            veh_prop_snp=request.form['veh_prop_snp'],
-            veh_usr_id  =request.form['veh_usr_id'], 
-            veh_gps     =request.form['veh_gps'],
-            veh_rev_tec =request.form['veh_rev_tec'],
-            veh_soa_seg =request.form['veh_soa_seg'],
-            veh_seg_ctr =request.form['veh_seg_ctr'],
-            veh_seg_bro =request.form['veh_seg_bro'],
-            veh_seg_nro =request.form['veh_seg_nro'],
-            veh_seg_ini =request.form['veh_seg_ini'],
-            veh_rev_tec =request.form['veh_rev_tec'],
-            usr_sta     ='ACTIVO',
-            usr_sta_mod=curr_editor.upper(),
-            usr_sta_fec=now_dt)
-        db.session.add(vehicle)
-        db.session.commit()
-        flash("Vehículo añadido con éxito", "success")
-        return redirect(url_for('flota'))
+        id = request.form['veh_lic_pla']
+        veh_rs = Vehicles.query.filter_by(veh_lic_pla=id).first()
+        if veh_rs:
+            veh_ck = Users.query.filter_by(veh_lic_pla=id,usr_sta='ACTIVO').first()
+            if veh_ck:
+                flash("Vehiculo ya existe","danger")
+                return redirect(url_for('flota'))
+            else:
+                Vehicles.query.filter_by(veh_lic_pla=id).update(dict(
+                    veh_lic_pla =request.form['veh_lic_pla'], 
+                    veh_usg     =request.form['veh_usg'],
+                    veh_mar     =request.form['veh_mar'],
+                    veh_mod     =request.form['veh_mod'],
+                    veh_yea     =request.form['veh_yea'],
+                    veh_col     =request.form['veh_col'],
+                    veh_prop_snp=request.form['veh_prop_snp'],
+                    veh_usr_id  =request.form['veh_usr_id'], 
+                    veh_gps     =request.form['veh_gps'],
+                    veh_rev_tec =request.form['veh_rev_tec'],
+                    veh_soa_seg =request.form['veh_soa_seg'],
+                    veh_seg_ctr =request.form['veh_seg_ctr'],
+                    veh_seg_bro =request.form['veh_seg_bro'],
+                    veh_seg_nro =request.form['veh_seg_nro'],
+                    veh_seg_ini =request.form['veh_seg_ini'],
+                    usr_sta     ='ACTIVO',
+                    usr_sta_mod=curr_editor.upper(),
+                    usr_sta_fec=now_dt))
+                db.session.commit()
+                flash("Vehiculo añadido con éxito","success")
+                return redirect(url_for('flota'))
+        else:
+            vehicle = Vehicles(
+                veh_lic_pla =request.form['veh_lic_pla'], 
+                veh_usg     =request.form['veh_usg'],
+                veh_mar     =request.form['veh_mar'],
+                veh_mod     =request.form['veh_mod'],
+                veh_yea     =request.form['veh_yea'],
+                veh_col     =request.form['veh_col'],
+                veh_prop_snp=request.form['veh_prop_snp'],
+                veh_usr_id  =request.form['veh_usr_id'], 
+                veh_gps     =request.form['veh_gps'],
+                veh_rev_tec =request.form['veh_rev_tec'],
+                veh_soa_seg =request.form['veh_soa_seg'],
+                veh_seg_ctr =request.form['veh_seg_ctr'],
+                veh_seg_bro =request.form['veh_seg_bro'],
+                veh_seg_nro =request.form['veh_seg_nro'],
+                veh_seg_ini =request.form['veh_seg_ini'],
+                veh_rev_tec =request.form['veh_rev_tec'],
+                usr_sta     ='ACTIVO',
+                usr_sta_mod=curr_editor.upper(),
+                usr_sta_fec=now_dt)
+            db.session.add(vehicle)
+            db.session.commit()
+            flash("Vehículo añadido con éxito", "success")
+            return redirect(url_for('flota'))
     else:
         redirect(url_for('login'))
 
@@ -259,7 +283,7 @@ def apieditvehicle():
         user_rs = Vehicles.query.filter_by(veh_lic_pla=id).first()
         if not user_rs:
             flash("No se pudo actualizar","danger")
-            return render_template('listado.html')
+            return redirect(url_for('flota'))
         else:
             Vehicles.query.filter_by(veh_lic_pla=id).update(dict(
                 veh_usg     =request.form['veh_usg'],
@@ -284,156 +308,3 @@ def apieditvehicle():
             return redirect(url_for('usuarios'))
     else:
         redirect(url_for('login'))
-
-# @api.route('/apication/users','/apication/users/')
-# class GetAndPost(Resource):
-#     # GET ALL
-#     def get(self):
-#         users_list = Users.query.all()
-#         result = users_schema.dump(users_list)
-#         return jsonify(result)
-    
-#     # POST
-#     def post(self):
-#         data    = api.payload
-#         now_dt  = datetime.now()
-#         user = Users(usr_id     =data['usr_id'], 
-#                      usr_tip_doc=data['usr_tip_doc'],
-#                      usr_nom    =data['usr_nom'],
-#                      usr_ape_pat=data['usr_ape_pat'],
-#                      usr_ape_mat=data['usr_ape_mat'],
-#                      usr_emp    =data['usr_emp'],
-#                      usr_tel_cel=data['usr_tel_cel'],
-#                      usr_dir    =data['usr_dir'], 
-#                      usr_cor_ele=data['usr_cor_ele'],
-#                      usr_sta    =data['usr_sta'],
-#                      usr_sta_mod=data['usr_sta_mod'],
-#                      usr_sta_fec=now_dt)
-#         user.save()
-#         user_added = Users.query.filter_by(usr_id=data['usr_id']).first()
-#         result = user_schema.dump(user_added)
-#         return jsonify(result)
-    
-# @api.route('/apication/user/<string:idx>','/apication/user/<string:idx>/')
-# class GetUpdateDelete(Resource):
-#     # GET ONE
-#     def get(self, idx):
-#         user_list = Users.query.filter_by(usr_id=idx).first()
-#         result = user_schema.dump(user_list)
-#         return jsonify(result)
-    
-#     # PUT
-#     def put(self, idx):
-#         data = api.payload
-#         now_dt = datetime.now()
-#         user_mod = Users.query.filter_by(usr_id=idx).first()
-#         if user_mod:
-#             Users.query.filter_by(usr_id=idx).update(dict(
-#                 usr_tip_doc=data['usr_tip_doc'],
-#                 usr_nom    =data['usr_nom'],
-#                 usr_ape_pat=data['usr_ape_pat'],
-#                 usr_ape_mat=data['usr_ape_mat'],
-#                 usr_emp    =data['usr_emp'],
-#                 usr_tel_cel=data['usr_tel_cel'],
-#                 usr_dir    =data['usr_dir'], 
-#                 usr_cor_ele=data['usr_cor_ele'],
-#                 usr_sta    =data['usr_sta'],
-#                 usr_sta_mod=data['usr_sta_mod'],
-#                 usr_sta_fec=now_dt
-#             ))
-#             db.session.commit()
-#             return jsonify(message="Update very succesful")
-#         else:
-#             return jsonify(message="That user does not exist"),404
-    
-#     # DELETE (TOO DANGEROUS)
-#     #def delete(self, idx):
-#     #    Users.objects(user_id=idx).delete()
-#     #    return jsonify('User is deleted')
-
-# # Vehicles -------------------------------------------------------------
-# @api.route('/apication/vehicles','/apication/vehicles/')
-# class GetAndPost(Resource):
-#     # GET ALL
-#     def get(self):
-#         vehicles_list = Vehicles.query.all()
-#         result = vehicles_schema.dump(vehicles_list)
-#         return jsonify(result)
-    
-#     # POST
-#     def post(self):
-#         data = api.payload
-#         curr_user = Users.query(usr_id=data['usr_id']).first()
-#         if curr_user:
-#             vehicle = Vehicles(veh_lic_pla=data['veh_lic_pla'], 
-#                                veh_usg=data['veh_usg'],
-#                                veh_mar=data['veh_mar'],
-#                                veh_mod=data['veh_mod'],
-#                                veh_yea=data['veh_yea'],
-#                                veh_col=data['veh_col'],
-#                                veh_prop_snp=data['veh_prop_snp'],
-#                                veh_usr_id=data['veh_usr_id'],
-#                                veh_gps=data['veh_gps'],
-#                                veh_rev_tec=data['veh_rev_tec'],
-#                                veh_soa_seg=data['veh_soa_seg'],
-#                                veh_seg_ctr=data['veh_seg_ctr'],
-#                                veh_seg_bro=data['veh_seg_bro'],
-#                                veh_seg_nro=data['veh_seg_nro'],
-#                                veh_seg_ini=data['veh_seg_ini'],
-#                                veh_sta=data['veh_sta'],
-#                                veh_sta_mod=data['veh_sta_mod'],
-#                                veh_sta_fec=data['veh_sta_fec'])
-#             vehicle.save()
-#             vehicle_added = Vehicles.query.\
-#                 filter_by(veh_lic_pla=data['usr_id']).first()
-#             result = vehicle_schema.dump(vehicle_added)
-#             return jsonify(result)
-        
-# @api.route('/apication/vehicle/<string:idx>',
-#            '/apication/vehicle/<string:idx>/')
-# class GetUpdateDelete(Resource):
-#     # GET ONE
-#     def get(self, idx):
-#         vehicle_list = Vehicles.query.filter_by(veh_lic_pla=idx).first()
-#         result = vehicle_schema.dump(vehicle_list)
-#         return jsonify(result)
-    
-#     # PUT
-#     def put(self, idx):
-#         data = api.payload
-#         Vehicles.query.filter_by(veh_lic_pla=idx).update(**data)
-#         vehicle_list = Vehicles.query.filter_by(veh_lic_pla=idx).first()
-#         result = vehicle_schema.dump(vehicle_list)
-#         return jsonify(result)
-    
-#     # DELETE (TOO DANGEROUS)
-#     #def delete(self, idx):
-#     #    Users.objects(user_id=idx).delete()
-#     #    return jsonify('User is deleted')
-
-# # Records --------------------------------------------------------------
-# @api.route('/apication/records','/apication/records/')
-# class GetAndPost(Resource):
-#     # GET ALL
-#     def get(self):
-#         return jsonify(Records.query.all())
-    
-#     # POST
-#     def post(self):
-#         data = api.payload
-#         curr_user   = Users.query.\
-#             filter_by(usr_id=data['rec_usr_id']).first()
-#         curr_veh    = Vehicles.query.\
-#             filter_by(veh_lic_pla=data['rec_veh_pla']).first()
-#         if curr_user and curr_veh:
-#             user_mix= curr_user['usr_nom'] + ' ' + curr_user['usr_ape_pat']
-#             veh_mix = curr_veh['veh_mar'] + ' ' + curr_veh['veh_mod'] 
-#             vehicle = Vehicles(rec_dat=data['rec_dat'], 
-#                                rec_veh_pla=data['rec_veh_pla'],
-#                                rec_veh_mix=veh_mix,
-#                                rec_usr_id=data['rec_usr_id'],
-#                                rec_usr_mix=user_mix,
-#                                rec_veh_loc=data['rec_veh_loc'],
-#                                rec_veh_des=data['rec_veh_des'])
-#             vehicle.save()
-#             return jsonify(Vehicles.query.order_by('-rec_id').first())
